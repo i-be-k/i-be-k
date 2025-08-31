acb1454af318290d66294e5724dde948b65f47dc
unknown <Opierah@Opsydev.(none)>
2025-08-31 17:19:58 +0100

    first Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,130 @@
+/**
+ * @copyright 2025 ibkod
+ * @license Apache-2.0
+ */
+
+/**
+ * Components
+ */
+import { ButtonPrimary } from "./Button";
+
+const sitemap = [
+  {
+    label: "Home",
+    href: "#home",
+  },
+  {
+    label: "About",
+    href: "#about",
+  },
+  {
+    label: "Work",
+    href: "#work",
+  },
+  {
+    label: "Reviews",
+    href: "#reviews",
+  },
+  {
+    label: "Contact me",
+    href: "#contact",
+  },
+];
+
+const socials = [
+  {
+    label: "GitHub",
+    href: "https://github.com/i-bee-k",
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/ibukun-obideyi-b06044135/",
+  },
+  {
+    label: "Twitter X",
+    href: "",
+  },
+  {
+    label: "Instagram",
+    href: "",
+  },
+];
+
+const Footer = () => {
+  return (
+    <footer className="section">
+      <div className="container">
+        <div className="lg:grid lg:grid-cols-2">
+          <div className="mb-10">
+            <h2 className="headline-1 mb-8 lg:max-w-[12ch] reveal-up">
+              Let&apos;s work together today!
+            </h2>
+
+            <ButtonPrimary
+              href="mailto:ibkodify@gmail.com"
+              label="Start project"
+              icon="chevron_right"
+              classes="reveal-up"
+            />
+          </div>
+
+          <div className="grid grid-cols-2 gap-4 lg:pl-20 text-slate-500">
+            <div>
+              <p className="mb-2 reveal-up">Sitemap</p>
+
+              <ul>
+                {sitemap.map(({ label, href }, key) => (
+                  <li key={key}>
+                    <a
+                      href={href}
+                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-slate-500 reveal-up"
+                    >
+                      {label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div>
+              <p className="mb-2 reveal-up">Socials</p>
+
+              <ul>
+                {socials.map(({ label, href }, key) => (
+                  <li key={key}>
+                    <a
+                      href={href}
+                      target="_blank"
+                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up"
+                    >
+                      {label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between pt-10 mb-8">
+          <a
+            href="/"
+            className="logo reveal-up">
+            <img
+              src="/images/ibkod-logo.png"
+              width={40}
+              height={40}
+              alt="Logo"
+            />
+          </a>
+
+          <p className="text-zinc-500 text-sm reveal-up">
+            &copy; 2025 <span className="text-slate-400">i-bee-k</span>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Skill.jsx
@@ -0,0 +1,131 @@
+/**
+ * @copyright 2025 i-bee-k
+ * @license Apache-2.0
+ */
+
+/**
+ * Components
+ */
+import SkillCard from "./SkillCard";
+
+const skillItem = [
+  {
+    imgSrc: "/images/html5.svg",
+    label: "HTML5",
+    desc: "Web-App Structure",
+  },
+  {
+    imgSrc: "/images/css3.svg",
+    label: "CSS",
+    desc: "Styles User Interface",
+  },
+  {
+    imgSrc: "/images/javascript.svg",
+    label: "JavaScript",
+    desc: "Interaction Function",
+  },
+  {
+    imgSrc: "/images/tailwindcss.svg",
+    label: "TailwindCSS",
+    desc: "Styles Framework",
+  },
+  {
+    imgSrc: "/images/sass.svg",
+    label: "SASS",
+    desc: "Styles Preprocessor",
+  },
+  {
+    imgSrc: "/images/react.svg",
+    label: "React",
+    desc: "JS Framework",
+  },
+  {
+    imgSrc: "/images/vitejs.svg",
+    label: "Vite",
+    desc: "Build tool + development server",
+  },
+  {
+    imgSrc: "/images/expressjs.svg",
+    label: "ExpressJS",
+    desc: "Node Framework",
+  },
+  {
+    imgSrc: "/images/nodejs.svg",
+    label: "NodeJS",
+    desc: "Web Server",
+  },
+  {
+    imgSrc: "/images/mongodb.svg",
+    label: "MongoDB",
+    desc: "Schema Database",
+  },
+  {
+    imgSrc: "/images/postman.svg",
+    label: "Postman",
+    desc: "API Testing Platform",
+  },
+  {
+    imgSrc: "/images/figma.svg",
+    label: "Figma",
+    desc: "Design tool",
+  },
+  {
+    imgSrc: "/images/c.svg",
+    label: "C",
+    desc: "General Purpose Build Tool",
+  },
+  {
+    imgSrc: "/images/python.svg",
+    label: "Python",
+    desc: "Multipurpose Build Tool",
+  },
+  {
+    imgSrc: "/images/github.svg",
+    label: "Github",
+    desc: "Cloud-Based Host",
+  },
+  {
+    imgSrc: "/images/git.svg",
+    label: "Git",
+    desc: "Version Control System",
+  },
+  {
+    imgSrc: "/images/bash.svg",
+    label: "Bash",
+    desc: "Command Line Interface",
+  },
+  {
+    imgSrc: "/images/netlify.svg",
+    label: "Netlify",
+    desc: "Build & Deploy",
+  },
+];
+
+const Skill = () => {
+  return (
+    <section className="section">
+      <div className="container">
+        <h2 className="headline-2 reveal-up">Indispensable Tools I use</h2>
+
+        <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch]">
+          Locate the formidable tools and technologies I use to create unusual,
+          high-performing websites & applications.
+        </p>
+
+        <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
+          {skillItem.map(({ imgSrc, label, desc }, key) => (
+            <SkillCard
+              key={key}
+              imgSrc={imgSrc}
+              label={label}
+              desc={desc}
+              className="reveal-up"
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skill;
